f1798b76c896532e094e6eabcc9f415fcd758d36
kooroshFarv <farvardin.koorosh@gmail.com>
2024-08-21 23:34:27 -0400

    added HomeProperties which has recent properties up to 3 and view all properties btn

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,13 @@ import Link from 'next/link'
 import Hero from 'components/hero'
 import InfoBoxes from 'components/InfoBoxes'
 import Footer from 'components/Footer'
+import HomeProperties from 'components/homeProperties'
 
 const HomePage = () => {
   return (
     <>
         < Hero />
+        < HomeProperties />
         < InfoBoxes />
         < Footer />
     </>
